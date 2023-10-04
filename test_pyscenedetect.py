997bf1d16a0accaca2c8a5acfba047003db13094
林嘉弘 <89115990+HarryandJack@users.noreply.github.com>
2023-10-04 18:49:35 +0800

    Present the images

--- test_pyscenedetect.py
+++
@@ -1,6 +0,0 @@
-from scenedetect import open_video, ContentDetector, SceneManager, StatsManager
-from scenedetect.frame_timecode import FrameTimecode
-from scenedetect.video_splitter import split_video_ffmpeg
-from scenedetect import detect, ContentDetector, split_video_ffmpeg
-import scenedetect
-scenedetect.video_splitter.is_ffmpeg_available()
